636f81f41fe27e4d48b4662477778b3017508d02
mmaryyoung <jyang@trinity.edu>
2017-07-18 15:02:17 -0500

    employs klepto separates getters to a diff file

--- getTags.py
+++ getTags.py
@@ -1,86 +1,8 @@
 import tables
 import os
-import pickle
 import numpy as np
-
-
-def get_title(h5,songidx=0):
-    """
-    Get title from a HDF5 song file, by default the first song in it
-    """
-    return h5.root.metadata.songs.cols.title[songidx]
-
-def get_danceability(h5,songidx=0):
-#     """
-#     Get danceability from a HDF5 song file, by default the first song in it
-#     """
-    return h5.root.analysis.songs.cols.danceability[songidx]
-
-def get_duration(h5,songidx=0):
-    """
-    Get duration from a HDF5 song file, by default the first song in it
-    """
-    return h5.root.analysis.songs.cols.duration[songidx]
-
-def get_num_songs(h5):
-    """
-    Return the number of songs contained in this h5 file, i.e. the number of rows
-    for all basic informations like name, artist, ...
-    """
-    return h5.root.metadata.songs.nrows
-
-def get_segments_timbre(h5,songidx=0):
-    """
-    Get segments timbre array. Takes care of the proper indexing if we are in aggregate
-    file. By default, return the array for the first song in the h5 file.
-    To get a regular numpy ndarray, cast the result to: numpy.array( )
-    """
-    if h5.root.analysis.songs.nrows == songidx + 1:
-        return h5.root.analysis.segments_timbre[h5.root.analysis.songs.cols.idx_segments_timbre[songidx]:,:]
-    return h5.root.analysis.segments_timbre[h5.root.analysis.songs.cols.idx_segments_timbre[songidx]:
-                                            h5.root.analysis.songs.cols.idx_segments_timbre[songidx+1],:]
-
-def get_segments_pitches(h5,songidx=0):
-    """
-    Get segments pitches array. Takes care of the proper indexing if we are in aggregate
-    file. By default, return the array for the first song in the h5 file.
-    To get a regular numpy ndarray, cast the result to: numpy.array( )
-    """
-    if h5.root.analysis.songs.nrows == songidx + 1:
-        return h5.root.analysis.segments_pitches[h5.root.analysis.songs.cols.idx_segments_pitches[songidx]:,:]
-    return h5.root.analysis.segments_pitches[h5.root.analysis.songs.cols.idx_segments_pitches[songidx]:
-                                             h5.root.analysis.songs.cols.idx_segments_pitches[songidx+1],:]
-
-def get_segments_start(h5,songidx=0):
-    """
-    Get segments start array. Takes care of the proper indexing if we are in aggregate
-    file. By default, return the array for the first song in the h5 file.
-    To get a regular numpy ndarray, cast the result to: numpy.array( )
-    """
-    if h5.root.analysis.songs.nrows == songidx + 1:
-        return h5.root.analysis.segments_start[h5.root.analysis.songs.cols.idx_segments_start[songidx]:]
-    return h5.root.analysis.segments_start[h5.root.analysis.songs.cols.idx_segments_start[songidx]:
-                                           h5.root.analysis.songs.cols.idx_segments_start[songidx+1]]
-
-# max_danceable_song = ''
-# max_dance = 0
-
-# for root, dirs, files in os.walk('/Users/mac/Downloads/MillionSongSubset/data'):
-# 	for name in files:
-# 		if '.h5' in name:
-# 			try:
-# 				h5 = tables.open_file(root + '/' + name, mode='r')
-# 				danceability = get_danceability(h5)
-# 				print danceability
-# 				
-# 				if danceability > max_dance:
-# 					max_dance = danceability
-# 					max_danceable_song = get_title(h5)
-#				h5.close()
-# 			except tables.exceptions.HDF5ExtError:
-# 				print "Can't find file"
-
-# print 'the most danceable song is ' + max_danceable_song
+from klepto.archives import *
+import getters
 
 x_train = []
 y_train = []
@@ -110,9 +32,9 @@ for line in tagsFile:
 			h5 = tables.open_file(dataRoot + lev1 +'/' + lev2 + '/' + lev3 + '/' + trackID + '.h5', mode='r')
 			oneLabel = [0]*15
 			oneLabel[genreID] = 1
-			for i in range(get_num_songs(h5)):
-				timbres = get_segments_timbre(h5, i)
-				pitches = get_segments_pitches(h5, i)
+			for i in range(getters.get_num_songs(h5)):
+				timbres = getters.get_segments_timbre(h5, i)
+				pitches = getters.get_segments_pitches(h5, i)
 				combo = np.concatenate((timbres,pitches),axis=1)
 				chunks = [combo[x:x+50] for x in range(0, len(combo)-50,50)]
 				chunks = np.expand_dims(chunks, axis=3)
@@ -126,8 +48,8 @@ for line in tagsFile:
 				else:
 					[x_holdout.append(x) for x in chunks]
 					[y_holdout.append(x) for x in [oneLabel]*len(chunks)]
-			#print get_title(h5), get_danceability(h5)
-			#print 'duration: ', get_duration(h5), 'timbre shape: ', get_segments_timbre(h5).shape, 'pitches shape: ', get_segments_pitches(h5).shape, 'segments start', get_segments_start(h5).shape
+			#print getters.get_title(h5), getters.get_danceability(h5)
+			#print 'duration: ', getters.get_duration(h5), 'timbre shape: ', getters.get_segments_timbre(h5).shape, 'pitches shape: ', getters.get_segments_pitches(h5).shape, 'segments start', getters.get_segments_start(h5).shape
 			rover +=1
 			h5.close()
 		except tables.exceptions.HDF5ExtError:
@@ -149,11 +71,21 @@ print 'y_test shape: ', y_test.shape
 print 'x_holdout shape: ', x_holdout.shape
 print 'y_holdout shape: ', y_holdout.shape
 
-pickle.dump(x_holdout, open('/data/hibbslab/jyang/msd/ver1.0/x_holdout.p', 'wb'))
-pickle.dump(y_holdout, open('/data/hibbslab/jyang/msd/ver1.0/y_holdout.p', 'wb'))
-pickle.dump(x_test, open('/data/hibbslab/jyang/msd/ver1.0/x_test.p', 'wb'))
-pickle.dump(y_test, open('/data/hibbslab/jyang/msd/ver1.0/y_test.p', 'wb'))
-pickle.dump(x_train, open('/data/hibbslab/jyang/msd/ver1.0/x_train.p', 'wb'))
-pickle.dump(y_train, open('/data/hibbslab/jyang/msd/ver1.0/y_train.p', 'wb'))
+arch = file_archive('/data/hibbslab/jyang/msd/ver1.0/klep')
+arch['x_train'] = x_train
+arch['y_train'] = y_train
+arch['x_test'] = x_test
+arch['y_test'] = y_test
+arch['x_holdout'] = x_holdout
+arch['y_holdout'] = y_holdout
+
+arch.dump()
+
+# pickle.dump(x_holdout, open('/data/hibbslab/jyang/msd/ver1.0/x_holdout.p', 'wb'))
+# pickle.dump(y_holdout, open('/data/hibbslab/jyang/msd/ver1.0/y_holdout.p', 'wb'))
+# pickle.dump(x_test, open('/data/hibbslab/jyang/msd/ver1.0/x_test.p', 'wb'))
+# pickle.dump(y_test, open('/data/hibbslab/jyang/msd/ver1.0/y_test.p', 'wb'))
+# pickle.dump(x_train, open('/data/hibbslab/jyang/msd/ver1.0/x_train.p', 'wb'))
+# pickle.dump(y_train, open('/data/hibbslab/jyang/msd/ver1.0/y_train.p', 'wb'))
 
 
