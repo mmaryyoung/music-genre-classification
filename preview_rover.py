68dc7f9b67b09f8cb2c0802b9f770ae048d98276
Mary Yang <838544531@qq.com>
2017-08-24 11:58:52 -0500

    sanity check

--- preview_rover.py
+++ preview_rover.py
@@ -1,4 +1,6 @@
 import os
+import pickle
+intactFiles = []
 
 for root, dirs, files in os.walk('/data/hibbslab/data/millionSong'):
 	for name in files:
@@ -7,7 +9,9 @@ for root, dirs, files in os.walk('/data/hibbslab/data/millionSong'):
 			try:
 				os.system('python get_preview_url.py 7dqyaqprpze7 ' + root + '/' + name)
 				print('yay we got one! ' + root + '/' + name)
+				intactFiles.append(root + '/' + name)
 			except urllib2.HTTPError:
-				pass
-
+				print('useless trash...' + root + '/' + name)
+print('Intact File Number: ' + len(intactFiles))
+pickle.dump(np.asarray(intactFiles), open('/home/jyang/summer2017/intactFiles.txt', 'wb'))
 
