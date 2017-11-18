9bc4838714828f6594ab6332c6c548d4f8b2c7c6
mmaryyoung <jyang@trinity.edu>
2017-11-18 02:54:55 -0600

    changed getTages parser

--- getTags.py
+++ getTags.py
@@ -1,16 +1,9 @@
 import tables
 import os
+import os.path
 import numpy as np
-from klepto.archives import *
-import sPickle
-import getters
-
-x_train = []
-y_train = []
-x_test = []
-y_test = []
-x_holdout = []
-y_holdout = []
+import librosa
+from melParseMSD import parseAudio
 
 
 # I MANUALLY WENT THROUGH THE TAGGER FILE AND GOT THIS
@@ -19,9 +12,9 @@ tags = ['Reggae', 'Jazz', 'RnB', 'Metal', 'Pop', 'Punk', 'Country', 'Latin', 'Ne
 # TENTATIVELY 1 - 95,700 TRAIN, 95,701 - 153,120 TEST, 153121 - END HOLDOUT
 tagsPath = '/home/jyang/summer2017/msd_tagtraum_cd2c.cls'
 tagsFile = open(tagsPath, 'r')
-dataRoot = '/data/hibbslab/data/millionSong/'
+sourceRoot = "/data/hibbslab/eherbert/millionSong/"
 
-rover = 0
+
 for line in tagsFile:
 	if line[:2] == 'TR':
 		trackID, genre = line.split('\t')
@@ -29,63 +22,20 @@ for line in tagsFile:
 		lev1 = trackID[2]
 		lev2 = trackID[3]
 		lev3 = trackID[4]
-		try:
-			h5 = tables.open_file(dataRoot + lev1 +'/' + lev2 + '/' + lev3 + '/' + trackID + '.h5', mode='r')
-			oneLabel = [0]*15
-			oneLabel[genreID] = 1
-			for i in range(getters.get_num_songs(h5)):
-				timbres = getters.get_segments_timbre(h5, i)
-				pitches = getters.get_segments_pitches(h5, i)
-				combo = np.concatenate((timbres,pitches),axis=1)
-				chunks = [combo[x:x+50] for x in range(0, len(combo)-50,50)]
-				chunks = np.expand_dims(chunks, axis=3)
-				#print chunks.shape
-				if rover < 95700:
-					[x_train.append(x) for x in chunks]
-					[y_train.append(x) for x in [oneLabel]*len(chunks)]
-				elif rover < 153120:
-					[x_test.append(x) for x in chunks]
-					[y_test.append(x) for x in [oneLabel]*len(chunks)]
-				else:
-					[x_holdout.append(x) for x in chunks]
-					[y_holdout.append(x) for x in [oneLabel]*len(chunks)]
-			#print getters.get_title(h5), getters.get_danceability(h5)
-			#print 'duration: ', getters.get_duration(h5), 'timbre shape: ', getters.get_segments_timbre(h5).shape, 'pitches shape: ', getters.get_segments_pitches(h5).shape, 'segments start', getters.get_segments_start(h5).shape
-			rover +=1
-			h5.close()
-		except tables.exceptions.HDF5ExtError:
-			print "Can't find file", trackID
-		except IOError:
-			print "Can't find file ", trackID 
 
-x_train = np.asarray(x_train)
-y_train = np.asarray(y_train)
-x_test = np.asarray(x_test)
-y_test = np.asarray(y_test)
-x_holdout = np.asarray(x_holdout)
-y_holdout = np.asarray(y_holdout)
+		genreRoot = '/data/hibbslab/jyang/msd/genres/' + genre[:-1] + '/'
+		sourcePath = sourceRoot + sourceRoot + lev1 + '/' + lev2 + '/' + lev3 + '/' + trackID + '.mp3'
 
-print 'x_train shape: ', x_train.shape
-print 'y_train shape: ', y_train.shape
-print 'x_test shape: ', x_test.shape
-print 'y_test shape: ', y_test.shape
-print 'x_holdout shape: ', x_holdout.shape
-print 'y_holdout shape: ', y_holdout.shape
+		if not os.path.isdir(genreRoot):
+			os.mkdirs(genreRoot)
 
-pickleRoot = '/data/hibbslab/jyang/msd/ver1.0/'
 
-sPickle.s_dump(x_holdout, open(pickleRoot + 'x_holdout.p', 'wb'))
-sPickle.s_dump(y_holdout, open(pickleRoot + 'x_holdout.p', 'wb'))
-sPickle.s_dump(x_test, open(pickleRoot + 'x_test.p', 'wb'))
-sPickle.s_dump(y_test, open(pickleRoot + 'y_test.p', 'wb'))
-sPickle.s_dump(x_train, open(pickleRoot + 'x_train.p', 'wb'))
-sPickle.s_dump(y_train, open(pickleRoot + 'y_train.p', 'wb'))
+		# If the file was not parse and we have a mp3 version
+		if not os.path.isfile(genreRoot + trackID) and  os.path.isfile(sourcePath):
+			parsed = parseAudio(sourcePath)
+			destPath = genreRoot + trackID + ".p"
+			pickle.dump(parsed, open(destPath, "wb"))
+			print "parsed file " + trackID + " as " + genre[:-1]
 
-# pickle.dump(x_holdout, open('/data/hibbslab/jyang/msd/ver1.0/x_holdout.p', 'wb'))
-# pickle.dump(y_holdout, open('/data/hibbslab/jyang/msd/ver1.0/y_holdout.p', 'wb'))
-# pickle.dump(x_test, open('/data/hibbslab/jyang/msd/ver1.0/x_test.p', 'wb'))
-# pickle.dump(y_test, open('/data/hibbslab/jyang/msd/ver1.0/y_test.p', 'wb'))
-# pickle.dump(x_train, open('/data/hibbslab/jyang/msd/ver1.0/x_train.p', 'wb'))
-# pickle.dump(y_train, open('/data/hibbslab/jyang/msd/ver1.0/y_train.p', 'wb'))
 
 
