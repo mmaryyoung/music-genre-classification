269643f28e6f7b06ba7385f292a6de65d6dc8569
mmaryyoung <jyang@trinity.edu>
2018-02-22 01:31:23 -0600

    torchRnn for music initial

--- torchRnn.py
+++ torchRnn.py
@@ -1,69 +1,36 @@
 # coding: utf-8
-########## PREPARE THE DATA ##########
 
-from __future__ import unicode_literals, print_function, division
-from io import open
-import glob
-
-def findFiles(path): return glob.glob(path)
-
-print(findFiles('data/names/*.txt'))
-
-import unicodedata
-import string
-
-all_letters = string.ascii_letters + " .,;'"
-n_letters = len(all_letters)
-
-# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-        and c in all_letters
-    )
-
-print(unicodeToAscii('Ślusàrski'))
-
-# Build the category_lines dictionary, a list of names per language
-category_lines = {}
-all_categories = []
-
-# Read a file and split into lines
-def readLines(filename):
-    lines = open(filename, encoding='utf-8').read().strip().split('\n')
-    return [unicodeToAscii(line) for line in lines]
-
-for filename in findFiles('data/names/*.txt'):
-    category = filename.split('/')[-1].split('.')[0]
-    all_categories.append(category)
-    lines = readLines(filename)
-    category_lines[category] = lines
-
-n_categories = len(all_categories)
-
-########## TURN NAMES INTO TENSORS ##########
-
+import pickle
+import numpy as np
 import torch
+import random
 
-# Find letter index from all_letters, e.g. "a" = 0
-def letterToIndex(letter):
-    return all_letters.find(letter)
 
-# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
-def letterToTensor(letter):
-    tensor = torch.zeros(1, n_letters)
-    tensor[0][letterToIndex(letter)] = 1
-    return tensor
 
-# Turn a line into a <line_length x 1 x n_letters>,
-# or an array of one-hot letter vectors
-def lineToTensor(line):
-    tensor = torch.zeros(len(line), 1, n_letters)
-    for li, letter in enumerate(line):
-        tensor[li][0][letterToIndex(letter)] = 1
-    return tensor
+sourceRoot = "/data/hibbslab/jyang/tzanetakis/ver6.0/"
+x_train = pickle.load(open(sourceRoot+"x_train.p", "rb"))
+y_train = pickle.load(open(sourceRoot+"y_train.p", "rb"))
 
+x_train = torch.from_numpy(x_train)
+y_train = torch.from_numpy(y_train)
+
+x_train_shape = list(x_train.size())
+
+# 5 sec out of 30 sec, a sixth
+all_genres = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
+n_genres = len(all_genres)
+n_features = x_train_shape[2]
+sample_length = x_train_shape[1]//6
+
+def randomTrainingExample():
+    
+    idx = random.randint(0, x_train_shape[0]-1)
+    jdx = random.randint(0, x_train_shape[1]-sample_length-1)
+    song_tensor = x_train[idx][jdx:jdx+sample_length].unsqueeze(1)
+    genre_tensor = y_train[idx]
+    song = "song" + idx
+    genre = all_genres[torch.nonzero(genre_tensor)[0][0]]
+    return genre, song, genre_tensor, song_tensor
 
 ########## CREATING THE NETWORK ##########
 
@@ -79,7 +46,7 @@ class RNN(nn.Module):
 
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
         self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax()
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
         combined = torch.cat((input, hidden), 1)
@@ -93,30 +60,18 @@ class RNN(nn.Module):
 
 # CHANGE HERE
 n_hidden = 128
-rnn = RNN(n_letters, n_hidden, n_categories)
+rnn = RNN(n_features, n_hidden, n_genres)
 
 
 ########## PREPARE FOR TRAINING ##########
-def categoryFromOutput(output):
+def genreFromOutput(output):
     top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
-    category_i = top_i[0][0]
-    return all_categories[category_i], category_i
-
-import random
-
-def randomChoice(l):
-    return l[random.randint(0, len(l) - 1)]
-
-def randomTrainingExample():
-    category = randomChoice(all_categories)
-    line = randomChoice(category_lines[category])
-    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
-    line_tensor = Variable(lineToTensor(line))
-    return category, line, category_tensor, line_tensor
+    genre_i = top_i[0][0]
+    return all_genres[genre_i], genre_i
 
 for i in range(10):
-    category, line, category_tensor, line_tensor = randomTrainingExample()
-    print('category =', category, '/ line =', line)
+    genre, song, genre_tensor, song_tensor = randomTrainingExample()
+    print('genre =', genre, '/ song =', song)
 
 ########## TRAINING THE NETWORK ##########
 criterion = nn.NLLLoss()
@@ -170,7 +125,7 @@ for iter in range(1, n_iters + 1):
 
     # Print iter number, loss, name and guess
     if iter % print_every == 0:
-        guess, guess_i = categoryFromOutput(output)
+        guess, guess_i = genreFromOutput(output)
         correct = '✓' if guess == category else '✗ (%s)' % category
         print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
 
@@ -179,76 +134,5 @@ for iter in range(1, n_iters + 1):
         all_losses.append(current_loss / plot_every)
         current_loss = 0
 
-torch.save(rnn.state_dict(), "./nameModel.m")
+#torch.save(rnn.state_dict(), "./nameModel.m")
 
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-
-plt.figure()
-plt.plot(all_losses)
-#plt.show()
-
-
-########## EVALUATING THE RESULTS ##########
-# Keep track of correct guesses in a confusion matrix
-confusion = torch.zeros(n_categories, n_categories)
-n_confusion = 10000
-
-# Just return an output given a line
-def evaluate(line_tensor):
-    hidden = rnn.initHidden()
-
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
-
-    return output
-
-# Go through a bunch of examples and record which are correctly guessed
-for i in range(n_confusion):
-    category, line, category_tensor, line_tensor = randomTrainingExample()
-    output = evaluate(line_tensor)
-    guess, guess_i = categoryFromOutput(output)
-    category_i = all_categories.index(category)
-    confusion[category_i][guess_i] += 1
-
-# Normalize by dividing every row by its sum
-for i in range(n_categories):
-    confusion[i] = confusion[i] / confusion[i].sum()
-
-# Set up plot
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(confusion.numpy())
-fig.colorbar(cax)
-
-# Set up axes
-ax.set_xticklabels([''] + all_categories, rotation=90)
-ax.set_yticklabels([''] + all_categories)
-
-# Force label at every tick
-ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-# sphinx_gallery_thumbnail_number = 2
-#plt.show()
-
-
-########## RUNNING ON USER INPUT ##########
-def predict(input_line, n_predictions=3):
-    print('\n> %s' % input_line)
-    output = evaluate(Variable(lineToTensor(input_line)))
-
-    # Get top N categories
-    topv, topi = output.data.topk(n_predictions, 1, True)
-    predictions = []
-
-    for i in range(n_predictions):
-        value = topv[0][i]
-        category_index = topi[0][i]
-        print('(%.2f) %s' % (value, all_categories[category_index]))
-        predictions.append([value, all_categories[category_index]])
-
-predict('Dovesky')
-predict('Jackson')
-predict('Hansen')
-predict('Sun')
