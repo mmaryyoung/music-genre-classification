d8fd1046498ed8358b3a18fa7ae210c9b0e1f441
mmaryyoung <jyang@trinity.edu>
2018-10-20 17:57:48 -0500

    updated waveform2data for all numpy processing

--- waveform2data.py
+++ waveform2data.py
@@ -9,12 +9,19 @@ import librosa.display
 source_path = "/data/hibbslab/jyang/genres/"
 dest_path = "/data/hibbslab/jyang/tzanetakis/ver8.0/"
 
-x_train = []
-y_train = []
-x_test = []
-y_test = []
-x_holdout = []
-y_holdout = []
+source_rate = 22050
+song_length = 30
+seconds_per_sample = 5
+samples_per_song = song_length/seconds_per_sample
+num_genres = 10
+songs_per_genre = 100
+
+x_train = np.empty([num_genres*50*samples_per_song, source_rate*seconds_per_sample])
+y_train = np.empty([num_genres*50*samples_per_song, num_genres])
+x_test = np.empty([num_genres*20*samples_per_song, source_rate*seconds_per_sample])
+y_test = np.empty([num_genres*20*samples_per_song, num_genres])
+x_holdout = np.empty([num_genres*30*samples_per_song, source_rate*seconds_per_sample])
+y_holdout = np.empty([num_genres*30*samples_per_song, num_genres])
 
 
 def parseAudio(genreIndex, songIndex, fName):
@@ -30,25 +37,27 @@ def parseAudio(genreIndex, songIndex, fName):
 		y = y[extraLength : audioLength + extraLength]
 	else:
 		audioLength = y.shape[0]
-	chunks = y
-	oneLabel = [0]*10
+	chunks = y.reshape([samples_per_song, -1])
+	oneLabel = np.zeros([num_genres])
 	oneLabel[genreIndex] = 1
 
 	#CHANGE HERE FOR HM DS
 	#CHANGE HERE FOR GTZAN
 	if songIndex < 50:
-		[x_train.append(x) for x in chunks]
-		[y_train.append(x) for x in [oneLabel]*len(chunks)]
+		x_train[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = chunks
+		y_train[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = np.full((samples_per_song,), oneLabel)
+		# [x_train.append(x) for x in chunks]
+		# [y_train.append(x) for x in [oneLabel]*len(chunks)]
 		# print('x_train: ', len(x_train), len(x_train[0]), len(x_train[0][0]))
 		# print('y_train: ', len(y_train))
 	elif songIndex > 70:
-		[x_holdout.append(x) for x in chunks]
-		[y_holdout.append(x) for x in [oneLabel]*len(chunks)]
+		x_holdout[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = chunks
+		y_holdout[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = np.full((samples_per_song,), oneLabel)
 		# print('x_holdout: ', len(x_holdout), len(x_holdout[0]), len(x_holdout[0][0]))
 		# print('y_holdout: ', len(y_holdout))
 	else:
-		[x_test.append(x) for x in chunks]
-		[y_test.append(x) for x in [oneLabel]*len(chunks)]
+		x_test[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = chunks
+		x_test[songIndex*samples_per_song:(songIndex+1)*samples_per_song] = np.full((samples_per_song,), oneLabel)
 		# print('x_test: ', len(x_test), len(x_test[0]), len(x_test[0][0]))
 		# print('y_test: ', len(y_test))
 	
@@ -74,9 +83,6 @@ for root, dirs, files in os.walk(source_path):
 x_train = np.asarray(x_train)
 x_test = np.asarray(x_test)
 x_holdout = np.asarray(x_holdout)
-#x_train /= np.amax(x_train)
-#x_test /= np.amax(x_test)
-#x_holdout /= np.amax(x_holdout)
 y_train = np.asarray(y_train)
 y_test = np.asarray(y_test)
 y_holdout = np.asarray(y_holdout)
