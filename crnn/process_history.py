280bbdf0c7b0ab298356c3fe82affdf3008a5f14
mmaryyoung <mmary.young@gmail.com>
2021-06-13 17:41:09 -0700

    automate the optimizer specs change through a config set

--- crnn/process_history.py
+++ crnn/process_history.py
@@ -5,11 +5,12 @@ def plot_history(history, title):
   training_loss = history.history['loss']
   validation_accuracy = history.history['val_categorical_accuracy']
   validation_loss = history.history['val_loss']
-  plt.plot(training_accuracy, label='training_acc', linestyle='-')
-  plt.plot(training_loss, label='training_loss', linestyle=':')
-  plt.plot(validation_accuracy, label='val_acc', linestyle='-')
-  plt.plot(validation_loss, label='val_loss', linestyle=':')
+  plt.plot(training_accuracy, label='training_acc', color='orange', linestyle='-')
+  plt.plot(training_loss, label='training_loss', color='red', linestyle=':')
+  plt.plot(validation_accuracy, label='val_acc', color='olive', linestyle='-')
+  plt.plot(validation_loss, label='val_loss', color='green', linestyle=':')
   plt.legend()
   plt.title(title)
-  plt.savefig(title + '.png')
+  filename =  title + '.png'
+  plt.savefig(filename.replace(' ', '_'))
   return plt
--- crnn/train_gtzan_crnn.py
+++ crnn/train_gtzan_crnn.py
@@ -1,10 +1,17 @@
 import numpy as np
+import tensorflow as tf
 from data_sanity_check import checkData
 from keras_crnn import createCRNNModel
 from keras.utils import np_utils
-from keras.optimizers import Adam, Nadam, RMSprop
+from keras.optimizers import Adadelta, Adagrad, Adamax, Adam, Nadam, RMSprop, SGD, Ftrl
 from process_history import plot_history
+
+import itertools
+import datetime
 import sys
+
+# Where the learning curve figures go.
+FIG_DIR_PATH = '/Users/maryyang/Learning/music-genre-classification/crnn/figs/'
 
 def loadall(filename=''):
     tmp = np.load(filename)
@@ -52,11 +59,49 @@ y_cv = np_utils.to_categorical(y_cv)
 
 if not checkData(x_tr, x_te, x_cv):
     sys.exit()
-opt = Nadam(learning_rate=0.0005)
 
-model = createCRNNModel(x_tr.shape[1:])
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['categorical_accuracy'])
-history = model.fit(x=x_tr, y=y_tr, batch_size=20, epochs=100, verbose=2, validation_data=(x_te, y_te), shuffle=True)
 
-plt = plot_history(history)
-plt.show()
+def variable_training(opt_type, learning_rate):
+    print('Running training with optimizer %s and learning rate of %f.' % (opt_type, learning_rate))
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_categorical_accuracy', patience=7)
+    if opt_type == 'adam':
+        opt = Adam(learning_rate=learning_rate)
+    elif opt_type == 'nadam':
+        opt = Nadam(learning_rate=learning_rate)
+    elif opt_type == 'sgd':
+        opt = SGD(learning_rate=learning_rate)
+    elif opt_type == 'adadelta':
+        opt = Adadelta(learning_rate=learning_rate)
+    elif opt_type == 'adagrad':
+        opt = Adagrad(learning_rate=learning_rate)
+    elif opt_type == 'adamax':
+        opt = Adamax(learning_rate=learning_rate)
+    elif opt_type == 'ftrl':
+        opt = Ftrl(learning_rate=learning_rate)
+    elif opt_type == 'rmsprop':
+        opt = RMSprop(learning_rate=learning_rate)
+    else:
+        raise NotImplementedError('Optimizer type %s is not implemented.' % opt_type)
+    opt = Nadam(learning_rate=0.0005)
+    model = createCRNNModel(x_tr.shape[1:])
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['categorical_accuracy'])
+    history = model.fit(
+        x=x_tr, y=y_tr,
+        validation_data=(x_te, y_te),
+        batch_size=20,
+        epochs=100,
+        callbacks=[early_stopping_callback],
+        verbose=2,
+        shuffle=True)
+    print('Done with this model spec. Best validation accuracy is %f.' % max(history.history['val_categorical_accuracy']))
+    batch_timestamp = str(datetime.datetime.now())
+    fig_title = batch_timestamp + str(opt_type) + '-' + str(learning_rate)
+    plt = plot_history(history, fig_title)
+    print('Done saving the last learning curve. ')
+
+
+opt_types = ['adam', 'nadam', 'rmsprop', 'sgd', 'adadelta', 'adagrad', 'adamax', 'ftrl']
+learning_rates = [1, 0.1, 0.01, 0.001, 0.0005, 0.0001]
+
+for pair in itertools.product(opt_types, learning_rates):
+    variable_training(pair[0], pair[1])
